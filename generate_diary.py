8d6da60a4e1902c25910e1afa8ab8c9f3b6374a0
goku <goku@goku.goku>
2024-09-29 22:13:35 +0200

    new

---
+++ generate_diary.py
@@ -0,0 +1,61 @@
+
+import datetime
+import locale
+
+
+def generate_heading():
+    f = open("text_blocks/initial.tex", "r")
+    return f.read()
+
+
+def generate_intro(start, semanas):
+    f = open("text_blocks/intro.tex", "r")
+    intro =  f.read()
+    start_str = start.strftime("%b %d, %Y")
+
+    intro = intro.replace("DATE1", start_str)
+    intro = intro.replace("WEEKS", str(semanas))
+    return intro
+
+def generate_calendar_page(start, end):
+    f = open("text_blocks/week.tex", "r")
+    table =  f.read()
+    start_str = start.strftime("%b %d, %Y")
+    end_str = end.strftime("%b %d, %Y")
+
+    table = table.replace("DATE1", start_str)
+    table = table.replace("DATE2", end_str)
+    return table
+
+
+
+
+def main():
+
+    initial_date =  datetime.datetime.now()
+    number_of_weeks = 5
+
+    resulting_tex = ""
+
+    resulting_tex = generate_heading()
+
+    start = initial_date - datetime.timedelta(days=initial_date.weekday())
+    resulting_tex = resulting_tex + generate_intro(start, number_of_weeks)
+    
+    current_date = initial_date
+    for i in range(number_of_weeks):
+        start = current_date - datetime.timedelta(days=current_date.weekday())
+        end = start + datetime.timedelta(days=6)
+        current_date = end + datetime.timedelta(days=1)
+        table = generate_calendar_page(start,end)
+        resulting_tex = resulting_tex + table + "\n\\newpage"
+    
+    
+    resulting_tex = resulting_tex + "\\end{document}"
+
+
+    with open('result.tex', 'w') as f:
+        f.write(resulting_tex)
+main()
+
+
